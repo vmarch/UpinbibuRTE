681645b579e9da479c51a9f7d1ec23c0c3fdc0bb
Volodymyr Marchenko <marchenkoukr@gmail.com>
2020-11-01 21:18:29 +0100

    Increased: 1) build:gradle:4.1.0 2) distributionUrl=https\://services.gradle.org/distributions/gradle-6.5-bin

--- library/src/main/java/devtolife/upinbiburte/UpiRichEditor.java
+++ library/src/main/java/devtolife/upinbiburte/UpiRichEditor.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.Locale;
 
 /**
- * Copyright (C) 2018 VMarch
+ * Copyright (C) 2020 VMarch
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- library/src/main/java/devtolife/upinbiburte/Utils.java
+++ library/src/main/java/devtolife/upinbiburte/Utils.java
@@ -11,7 +11,7 @@ import android.util.Base64;
 import java.io.ByteArrayOutputStream;
 
 /**
- * Copyright (C) 2018 VMarch
+ * Copyright (C) 2020 VMarch
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
